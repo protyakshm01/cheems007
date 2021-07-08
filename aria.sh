a0b2dcba54149a999010e34a5e914994079f34d1
protyakshm01 <87114990+protyakshm01@users.noreply.github.com>
2021-07-08 12:27:53 +0530

    yes

---
+++ aria.sh
@@ -0,0 +1,15 @@
+TRACKERS=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_udp.txt | awk '$1' | tr '\n' ',')
+
+aria2c \
+--allow-overwrite=true \
+--bt-enable-lpd=true \
+--bt-max-peers=0 \
+--bt-tracker="[$TRACKERS]" \
+--bt-stop-timeout=500 \
+--check-certificate=false \
+--daemon=true \
+--enable-rpc \
+--follow-torrent=mem \
+--max-connection-per-server=16 \
+--max-overall-upload-limit=1K \
+--seed-time=0
